39b9ee5aced36e383251711e8d73d484e97bcc98
Mamory <32417036+Drift2020@users.noreply.github.com>
2019-01-11 16:53:49 +0200

    work servis

--- Chat/Chat_servis_library/Chat_servis.cs
+++ Chat/Chat_servis_library/Chat_servis.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace Chat_servis_library
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Chat_servis" in both code and config file together.
-
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class Chat_servis : IChat_servis
     {
         public int Connect()
---
+++ Chat/Chat_servis_library/ServerUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+ 
+namespace Chat_servis_library
+{
+    class ServerUser
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public int Name
+        {
+            get;
+            set;
+        }
+        public OperationContext operationContext
+        {
+            get;
+            set;
+        }
+    }
+}
